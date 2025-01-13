e9cfc6eb1196fc7ac7d667bbb8e05c0dbfceadc8
chycheung <chychueng@outlook.com>
2025-01-13 20:55:33 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -92,23 +92,3 @@ export const mainStore = defineStore("main", {
   },
 });
 
-let getResume = () => {
-  let items = [...document.querySelector(".item")];
-  for (let i of items) {
-    if (i.textContent === "简历") {
-      return i.previousElementSibling;
-    }
-  }
-}
-
-let resume = getResume();
-resume.links.addEventListener("onclick", () => {
-  let pwSet = 2002;
-  let pwInput = prompt("查看简历请输入密码：", "");
-  if (pwInput === null && pwInput !== pwSet) {
-    alert('密码错误');
-    window.location.href = 'https://sova.org.cn';
-  } else {
-    window.open("https://chycheung.me", '_blank');
-  }
-});
